4d25565179ed8968ba7e188411ca844c0796929e
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-03 09:19:40 +0100

    add day 3

---
+++ day03/README.md
@@ -0,0 +1,3 @@
+# Day 3: Binary Diagnostic
+
+Compute aggregates and search over a bit-value matrix. Read the [full problem description](https://adventofcode.com/2021/day/3).
---
+++ day03/main.py
@@ -0,0 +1,44 @@
+"""Day 3: Binary Diagnostic"""
+
+def gamma(report):
+    return bin2dec(bits_by_column(report, arg_max))
+
+def epsilon(report):
+    return bin2dec(bits_by_column(report, arg_min))
+
+def oxygen(report):
+    return bin2dec(filter_down(report, arg_max))
+
+def co2(report):
+    return bin2dec(filter_down(report, arg_min))
+
+def arg_max(bits):
+    return int(sum(bits) >= len(bits) / 2)
+
+def arg_min(bits):
+    return int(sum(bits) < len(bits) / 2)
+
+def bin2dec(bit_list):
+    return int(''.join(map(str, bit_list)), base=2)
+
+def transpose(matrix):
+    return list(zip(*matrix))
+
+def bits_by_column(report, criterion):
+    return [criterion(transpose(report)[i]) for i in range(len(report[0]))]
+
+def filter_down(report, criterion):
+    rest = report
+    for i in range(len(report)):
+        bit = criterion(transpose(rest)[i])
+        rest = [r for r in rest if r[i] == bit]
+        if len(rest) <= 1:
+            return rest[0]
+
+
+with open('input.txt') as f:
+    report = [[int(c) for c in list(l.strip())] for l in f.readlines()]
+
+    print('part 1:', gamma(report) * epsilon(report))
+    print('part 2:', oxygen(report) * co2(report))
+
