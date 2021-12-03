b029052d37b769b029f17fd264691fefadda1899
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-03 09:21:13 +0100

    fix: add missing main section

--- day03/main.py
+++ day03/main.py
@@ -36,8 +36,9 @@ def filter_down(report, criterion):
             return rest[0]
 
 
-with open('input.txt') as f:
-    report = [[int(c) for c in list(l.strip())] for l in f.readlines()]
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        report = [[int(c) for c in list(l.strip())] for l in f.readlines()]
 
     print('part 1:', gamma(report) * epsilon(report))
     print('part 2:', oxygen(report) * co2(report))
