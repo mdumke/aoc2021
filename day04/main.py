ad791f578fc97295fb1f007b51f8770ef2a1ac46
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-04 15:12:00 +0100

    simplify

--- day04/main.py
+++ day04/main.py
@@ -5,47 +5,42 @@ from itertools import chain, compress
 
 class Matrix(list):
     @staticmethod
-    def fill(fill_value, shape):
-        return Matrix([[fill_value for _ in range(shape[1])]
-                                   for _ in range(shape[0])])
+    def fill(value, shape):
+        return Matrix([[value] * shape[1] for _ in range(shape[0])])
 
-    @property
     def shape(self):
         return len(self), len(self[0])
-
-    def indices(self):
-        return ((i, j) for i in range(len(self))
-                       for j in range(len(self[0])))
 
     def flatten(self):
         return chain.from_iterable(self)
 
-    def get_row(self, row):
+    def row(self, row):
         return self[row]
 
-    def get_col(self, col):
+    def col(self, col):
         return list(zip(*self))[col]
 
     def find(self, value):
-        for i, j in self.indices():
-            if self[i][j] == value:
-                return i, j
+        for i in range(len(self)):
+            for j in range(len(self[0])):
+                if self[i][j] == value:
+                    return i, j
 
 
 class Board(Matrix):
     def __init__(self, numbers):
         Matrix.__init__(self, numbers)
         self.bingo = False
-        self.marks = Matrix.fill(1, shape=self.shape)
+        self.marks = Matrix.fill(1, shape=self.shape())
 
     def mark(self, number):
         if (pos := self.find(number)):
             self.marks[pos[0]][pos[1]] = 0
             self.check_bingo(pos[0], pos[1])
 
-    def check_bingo(self, row, col):
-        self.bingo |= sum(self.marks.get_row(row)) == 0 or \
-                      sum(self.marks.get_col(col)) == 0
+    def check_bingo(self, i, j):
+        self.bingo |= sum(self.marks.row(i)) == 0 or \
+                      sum(self.marks.col(j)) == 0
 
     def score(self):
         return sum(compress(self.flatten(), self.marks.flatten()))
