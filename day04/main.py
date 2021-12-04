833eb6ab70403091f4bd756c254571712b91eed4
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-04 20:58:23 +0100

    try a functional solution

--- day04/main.py
+++ day04/main.py
@@ -1,71 +1,44 @@
 """Day 4: Giant Squid"""
 
-from itertools import chain, compress
+from itertools import product
+from copy import deepcopy
 
+def mark(board, n):
+    board = deepcopy(board)
+    for i, j in product(range(5), repeat=2):
+        if board[i][j] == n:
+            board[5][5] -= n
+            board[i][5] += 1
+            board[5][j] += 1
+            break
+    return board
 
-class Matrix(list):
-    @staticmethod
-    def fill(value, shape):
-        return Matrix([[value] * shape[1] for _ in range(shape[0])])
+def is_bingo(board):
+    for i in range(5):
+        if board[i][5] == 5 or board[5][i] == 5:
+            return True
 
-    def shape(self):
-        return len(self), len(self[0])
+def play(board, draws):
+    for i, draw in enumerate(draws):
+        board = mark(board, draw)
+        if is_bingo(board):
+            return i, draw * board[5][5]
 
-    def flatten(self):
-        return chain.from_iterable(self)
-
-    def row(self, row):
-        return self[row]
-
-    def col(self, col):
-        return list(zip(*self))[col]
-
-    def find(self, value):
-        for i in range(len(self)):
-            for j in range(len(self[0])):
-                if self[i][j] == value:
-                    return i, j
-
-
-class Board(Matrix):
-    def __init__(self, numbers):
-        Matrix.__init__(self, numbers)
-        self.bingo = False
-        self.marks = Matrix.fill(1, shape=self.shape())
-
-    def mark(self, number):
-        if (pos := self.find(number)):
-            self.marks[pos[0]][pos[1]] = 0
-            self.check_bingo(pos[0], pos[1])
-
-    def check_bingo(self, i, j):
-        self.bingo |= sum(self.marks.row(i)) == 0 or \
-                      sum(self.marks.col(j)) == 0
-
-    def score(self):
-        return sum(compress(self.flatten(), self.marks.flatten()))
-
-
-def play(boards, draws, play_to_win=True):
-    for draw in draws:
-        boards = [b for b in boards if not b.bingo]
-        for board in boards:
-            board.mark(draw)
-            if board.bingo and play_to_win or len(boards) == 1:
-                return draw * board.score()
-
+def build_board(data):
+    return [[*line, 0] for line in data] + \
+           [[0, 0, 0, 0, 0, sum([sum(line) for line in data])]]
 
 def load_data(filename):
-    with open(filename) as f:
-        draws = [int(n) for n in f.readline().split(',')]
-        boards = []
-        while f.readline():
-            boards.append([[int(n) for n in f.readline().split()] for _ in range(5)])
-    return boards, draws
+    with open('input.txt') as f:
+        draws, *board_data = f.read().split('\n\n')
+    draws = [int(n) for n in draws.split(',')]
+    board_data = [build_board([[int(n) for n in line.split()]
+                           for line in b.strip().split('\n')])
+                           for b in board_data]
+    return draws, board_data
 
 
 if __name__ == '__main__':
-    boards_data, draws = load_data('input.txt')
-    print('part 1:', play(map(Board, boards_data), draws))
-    print('part 2:', play(map(Board, boards_data), draws, False))
-
+    draws, boards = load_data('input.txt')
+    print('part 1:', min([play(board, draws) for board in boards])[1])
+    print('part 2:', max([play(board, draws) for board in boards])[1])
