2c8df5c09d9aa22d66f7e4efb51bb61d9d2177a3
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-05 11:23:38 +0100

    simplify line representation

--- day05/main.py
+++ day05/main.py
@@ -1,55 +1,39 @@
 """Day 5: Hydrothermal Venture"""
 
-from collections import namedtuple, defaultdict
+from collections import defaultdict
 
-Point = namedtuple('Point', 'x y')
+def get_horizontal_coords(x1, x2, y):
+    return [(i, y) for i in range(min(x1, x2), max(x1, x2) + 1)]
 
-def get_horizontal_coords(p1, p2):
-    l, r = (p1, p2) if p1.x <= p2.x else (p2, p1)
-    return [(i, l.y) for i in range(l.x, r.x+1)]
+def get_vertical_coords(y1, y2, x):
+    return [(x, i) for i in range(min(y1, y2), max(y1, y2) + 1)]
 
-def get_vertical_coords(p1, p2):
-    lo, hi = (p1, p2) if p1.y <= p2.y else (p2, p1)
-    return [(lo.x, i) for i in range(lo.y, hi.y+1)]
+def get_diagonal_coords(x1, y1, x2, y2):
+    lx, ly, rx, ry = (x1, y1, x2, y2) if x1 <= x2 else (x2, y2, x1, y1)
+    slope = 1 if ly <= ry else -1
+    return [(lx + i, ly + i * slope) for i in range(rx - lx + 1)]
 
-def get_diagonal_coords(p1, p2):
-    l, r = (p1, p2) if p1.x <= p2.x else (p2, p1)
-    slope = 1 if l.y < r.y else -1
-    return [(l.x + i, l.y + slope * i) for i in range(r.x-l.x+1)]
-
-def build_map(lines, include_diagonals=False):
-    """returns a lookup of how many lines cover a coordinate"""
-    horizontals = [(p1, p2) for p1, p2 in lines if p1.y == p2.y]
-    verticals = [(p1, p2) for p1, p2 in lines if p1.x == p2.x]
-    diagonals = [(p1, p2) for p1, p2 in lines if p1.x != p2.x and p1.y != p2.y]
-
+def build_plan(lines, include_diagonals=False):
     plan = defaultdict(int)
 
-    for line in horizontals:
-        for point in get_horizontal_coords(*line):
-            plan[point] += 1
-
-    for line in verticals:
-        for point in get_vertical_coords(*line):
-            plan[point] += 1
-
-    if include_diagonals:
-        for line in diagonals:
-            for point in get_diagonal_coords(*line):
+    for x1, y1, x2, y2 in lines:
+        if y1 == y2:
+            for point in get_horizontal_coords(x1, x2, y1):
+                plan[point] += 1
+        elif x1 == x2:
+            for point in get_vertical_coords(y1, y2, x1):
+                plan[point] += 1
+        elif include_diagonals:
+            for point in get_diagonal_coords(x1, y1, x2, y2):
                 plan[point] += 1
 
     return plan
 
-def read_input(filename):
-    """returns lines in the form [[Point(1, 2), Point(5, 4)], ...]"""
-    with open(filename) as f:
-        return [[Point(*[int(n) for n in p.split(',')])
-                for p in line.strip().split(' -> ')]
-               for line in f.readlines()]
-
-
 if __name__ == '__main__':
-    lines = read_input('input.txt')
-    print('part 1:', sum([v > 1 for v in build_map(lines).values()]))
-    print('part 2:', sum([v > 1 for v in build_map(lines, True).values()]))
+    with open('input.txt') as f:
+        lines = [[int(n) for n in [*p1.split(','), *p2.split(',')]]
+                 for p1, p2 in [l.strip().split(' -> ') for l in f.readlines()]]
+
+    print('part 1:', sum([v > 1 for v in build_plan(lines).values()]))
+    print('part 2:', sum([v > 1 for v in build_plan(lines, True).values()]))
 
