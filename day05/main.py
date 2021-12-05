8869be297dfb5a7d7d5bb59f04869701c1f6e208
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-05 10:57:03 +0100

    make point assignments consistent

--- day05/main.py
+++ day05/main.py
@@ -5,20 +5,17 @@ from collections import namedtuple, defaultdict
 Point = namedtuple('Point', 'x y')
 
 def get_horizontal_coords(p1, p2):
-    l = min(p1.x, p2.x)
-    r = max(p1.x, p2.x)
-    return [Point(i, p1.y) for i in range(l, r+1)]
+    l, r = (p1, p2) if p1.x <= p2.x else (p2, p1)
+    return [(i, l.y) for i in range(l.x, r.x+1)]
 
 def get_vertical_coords(p1, p2):
-    lo = min(p1.y, p2.y)
-    hi = max(p1.y, p2.y)
-    return [Point(p1.x, i) for i in range(lo, hi+1)]
+    lo, hi = (p1, p2) if p1.y <= p2.y else (p2, p1)
+    return [(lo.x, i) for i in range(lo.y, hi.y+1)]
 
 def get_diagonal_coords(p1, p2):
-    l = p1 if p1.x <= p2.x else p2
-    r = p2 if l == p1 else p1
+    l, r = (p1, p2) if p1.x <= p2.x else (p2, p1)
     slope = 1 if l.y < r.y else -1
-    return [Point(l.x + i, l.y + slope * i) for i in range(r.x-l.x+1)]
+    return [(l.x + i, l.y + slope * i) for i in range(r.x-l.x+1)]
 
 def build_map(lines, include_diagonals=False):
     horizontals = [(p1, p2) for p1, p2 in lines if p1.y == p2.y]
