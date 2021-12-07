afc873203640eaedbcc17e481d20de77821c3dbc
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-07 07:39:26 +0100

    add day 7

---
+++ day07/README.md
@@ -0,0 +1,3 @@
+# Day 7: The Treachery of Whales
+
+Determine the most fuel efficient positioning of submarines. Read the [full problem description](https://adventofcode.com/2021/day/7).
---
+++ day07/main.py
@@ -0,0 +1,7 @@
+"""Day 7: The Treachery of Whales"""
+
+with open('input.txt') as f:
+    crabs = [int(n) for n in f.readline().split(',')]
+
+print('part 1:', min([sum([abs(c-i) for c in crabs]) for i in range(min(crabs), max(crabs))]))
+print('part 2:', min([sum([(abs(c-i)*(abs(c-i)+1))/2 for c in crabs]) for i in range(min(crabs), max(crabs))]))
