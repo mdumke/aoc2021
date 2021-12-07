e053f4e47e171b55c68418c14bbb8491a19dd3bb
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-07 08:54:57 +0100

    remove unnecessary list comprehensions

--- day07/main.py
+++ day07/main.py
@@ -3,5 +3,5 @@
 with open('input.txt') as f:
     crabs = [int(n) for n in f.readline().split(',')]
 
-print('part 1:', min([sum([abs(c-i) for c in crabs]) for i in range(min(crabs), max(crabs))]))
-print('part 2:', min([sum([(abs(c-i)*(abs(c-i)+1))/2 for c in crabs]) for i in range(min(crabs), max(crabs))]))
+print('part 1:', min(sum(abs(c-i) for c in crabs) for i in range(min(crabs), max(crabs))))
+print('part 2:', min(sum((abs(c-i)*(abs(c-i)+1))/2 for c in crabs) for i in range(min(crabs), max(crabs))))
