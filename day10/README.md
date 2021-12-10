5733f8483a3856f891b6b42a5e71b16fc038978f
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-10 08:41:29 +0100

    add day 10

---
+++ day10/README.md
@@ -0,0 +1,3 @@
+# Day 10: Syntax Scoring
+
+Analyse consisteny of strings of opening and closing brackets. Read the [full problem description](https://adventofcode.com/2021/day/10).
---
+++ day10/main.py
@@ -0,0 +1,50 @@
+"""Day 10: Syntax Scoring"""
+
+from collections import deque
+
+complements = {'(': ')', '[': ']', '{': '}', '<': '>'}
+corruption_scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+incomplete_scores ={')': 1, ']': 2, '}': 3, '>': 4}
+
+def check_syntax(line):
+    stack = deque()
+    for i, char in enumerate(line):
+        if char in complements:
+            stack.append(char)
+        else:
+            bracket = stack.pop()
+            if char != complements[bracket]:
+                return char, None
+    return None, stack
+
+def compute_corruption_score(lines):
+    total = 0
+    for line in lines:
+        char, _ = check_syntax(line)
+        if char:
+            total += corruption_scores[char]
+    return total
+
+def score_helper(stack):
+    score = 0
+    while len(stack):
+        score *= 5
+        score += incomplete_scores[complements[stack.pop()]]
+    return score
+
+def compute_incomplete_score(lines):
+    scores = []
+    for line in lines:
+        char, stack = check_syntax(line)
+        if char is None:
+            scores.append(score_helper(stack))
+    return sorted(scores)[len(scores) // 2]
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+    print('part 1:', compute_corruption_score(lines))
+    print('part 2:', compute_incomplete_score(lines))
+
