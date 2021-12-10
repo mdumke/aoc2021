3d521b10304fd44253a3b50e4c55193d390cbaa2
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-10 13:58:40 +0100

    refactor to be more functional

--- day10/main.py
+++ day10/main.py
@@ -1,40 +1,41 @@
 """Day 10: Syntax Scoring"""
 
+def median(numbers):
+    return sorted(numbers)[len(numbers) // 2]
 
 def check_syntax(line):
-    complements = dict(['()', '[]', '{}', '<>'])
     stack = []
     for char in line:
-        if char in complements:
-            stack.append(char)
-        else:
-            bracket = stack.pop()
-            if char != complements[bracket]:
-                return char, None
-    return None, stack
+        if char in '({[<':
+            stack.append(')]}>'['([{<'.index(char)])
+        elif stack.pop() != char:
+            return {'corrupted': char}
+    return {'missing': reversed(stack)}
 
-def compute_corruption_score(lines):
-    total = 0
-    for line in lines:
-        char, stack = check_syntax(line)
-        if char:
-            total += [3, 57, 1197, 25137][')]}>'.index(char)]
-    return total
-
-def score_helper(stack):
+def score_helper(brackets):
     score = 0
-    while len(stack):
+    for v in [incomplete_value(b) for b in brackets]:
         score *= 5
-        score += '([{<'.index(stack.pop()) + 1
+        score += v
     return score
 
+def corruption_value(bracket):
+    return [3, 57, 1197, 25137][')}]>'.index(bracket)]
+
+def incomplete_value(bracket):
+    return [1, 2, 3, 4][')]}>'.index(bracket)]
+
+def map_corrupted_chars(lines, fn):
+    return [fn(c) for c in [check_syntax(l).get('corrupted') for l in lines] if c]
+
+def map_missing_brackets(lines, fn):
+    return [fn(b) for b in [check_syntax(l).get('missing') for l in lines] if b]
+
+def compute_corruption_score(lines):
+    return sum(map_corrupted_chars(lines, corruption_value))
+
 def compute_incomplete_score(lines):
-    scores = []
-    for line in lines:
-        char, stack = check_syntax(line)
-        if char is None:
-            scores.append(score_helper(stack))
-    return sorted(scores)[len(scores) // 2]
+    return median(map_missing_brackets(lines, score_helper))
 
 
 if __name__ == '__main__':
