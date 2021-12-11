5450d1d2d22ff0b2a812b9308f16447a24334ca2
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-11 11:55:57 +0100

    some refactoring

--- day11/main.py
+++ day11/main.py
@@ -1,72 +1,60 @@
 """Day 11: Dumbo Octopus"""
 
-def increment(grid):
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            grid[i][j] += 1
+def positions(grid):
+    return ((i, j) for i in range(len(grid))
+                   for j in range(len(grid[0])))
 
-def get_flash_positions(grid):
-    flash_positions = []
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] > 9:
-                flash_positions.append((i, j))
-    return flash_positions
+def increment(grid):
+    for i, j in positions(grid):
+        grid[i][j] += 1
+
+def flash_positions(grid):
+    return [(i, j) for i, j in positions(grid) if grid[i][j] > 9]
 
 def set_zeros(grid):
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] is None:
-                grid[i][j] = 0
+    for i, j in positions(grid):
+        if grid[i][j] is None:
+            grid[i][j] = 0
 
-def is_flashable(grid, i, j):
+def is_valid(grid, i, j):
     return i >= 0 and i < len(grid) and \
            j >= 0 and j < len(grid[0]) and \
            grid[i][j] is not None
 
 def neighbors(grid, i, j):
-    offsets = [(-1, -1), (-1, 0), (-1, 1),
-               ( 0, -1),          ( 0, 1),
-               ( 1, -1), ( 1, 0), ( 1, 1)]
-    return [(i+dx, j+dy) for dx, dy in offsets
-                         if is_flashable(grid, i+dx, j+dy)]
+    return filter(lambda pos: is_valid(grid, *pos),
+                 ((i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1),
+                  (i+1, j-1), (i+1, j), (i+1, j+1)))
 
 def flash_at(grid, i, j):
     grid[i][j] = None
-    new_positions = []
     for x, y in neighbors(grid, i, j):
         grid[x][y] += 1
-        if grid[x][y] > 9:
-            new_positions.append((x, y))
-    return new_positions
+    return [(x, y) for x, y in neighbors(grid, i, j) if grid[x][y] > 9]
 
-def flash(grid):
-    count = 0
-    pos = get_flash_positions(grid)
+def step(grid):
+    increment(grid)
+    pos = flash_positions(grid)
+    flash_count = 0
     while len(pos):
         i, j = pos.pop()
         if grid[i][j] is not None:
-            count += 1
+            flash_count += 1
             pos += flash_at(grid, i, j)
     set_zeros(grid)
-    return count
-
-def count_flashes(grid, steps):
-    flash_count = 0
-    for _ in range(steps):
-        increment(grid)
-        flash_count += flash(grid)
     return flash_count
 
-def is_syncronized(grid):
+def count_flashes(grid, steps):
+    return sum(step(grid) for _ in range(steps))
+
+def in_sync(grid):
     return sum(sum(row) for row in grid) == 0
 
 def find_first_sync(grid):
-    for i in range(100_000):
-        increment(grid)
-        flash(grid)
-        if is_syncronized(grid):
-            return i + 1
+    for n in range(1, 10000):
+        step(grid)
+        if in_sync(grid):
+            return n
 
 def load_grid(filename):
     with open(filename) as f:
