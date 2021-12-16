10e596123d045295290f01a53a0c564ba17ca760
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-16 18:07:18 +0100

    add day 16

---
+++ day16/README.md
@@ -0,0 +1,3 @@
+# Day 16: Packet Decoder
+
+Decode a binary string containing nested computations. Read the [full problem description](https://adventofcode.com/2021/day/16).
--- day16/main.py
+++ day16/main.py
@@ -1,3 +1,8 @@
+"""Packet Decoder"""
+
+import math
+from operator import gt, lt
+
 def hex_to_bin(n):
     return ''.join([str(bin(int(i, base=16)))[2:].rjust(4, '0') for i in n])
 
@@ -19,37 +24,64 @@ def decode_packet(bits):
     version_sum += version
 
     if type_id == 4:
-        lit, offset = decode_literal(bits[6:])
-        return None, offset + 6
-    elif int(bits[6], 2) == 0:
-        length = int(bits[7:22], 2)
-        _, offset = decode_sequence(bits[22:22+length])
-        return None, 22 + offset
+        n, offset = decode_literal(bits[6:])
+        return n, offset + 6
     else:
-        n_sub = int(bits[7:18], 2)
-        _, offset = decode_group(bits[18:], n_sub)
-        return None, 18 + offset
+        ns, offset = decode_packet_list(bits[6:])
+
+        if type_id == 0:
+            result = sum(ns)
+        if type_id == 1:
+            result = math.prod(ns)
+        if type_id == 2:
+            result = min(ns)
+        if type_id == 3:
+            result = max(ns)
+        if type_id == 5:
+            result = gt(*ns)
+        if type_id == 6:
+            result = lt(*ns)
+        if type_id == 7:
+            result = ns[0] == ns[1]
+
+        return result, 6 + offset
+
+def decode_packet_list(bits):
+    if int(bits[0], 2) == 0:
+        length = int(bits[1:16], 2)
+        ns, offset = decode_sequence(bits[16:16+length])
+        return ns, 16 + offset
+    else:
+        n_sub = int(bits[1:12], 2)
+        ns, offset = decode_group(bits[12:], n_sub)
+        return ns, 12 + offset
 
 def decode_sequence(bits):
+    values = []
     i = 0
     while i < len(bits):
-        _, offset = decode_packet(bits[i:])
+        v, offset = decode_packet(bits[i:])
         i += offset
-    return None, i
+        values.append(v)
+    return values, i
 
 def decode_group(bits, size):
     i = 0
+    values = []
     for packet in range(size):
-        res, offset = decode_packet(bits[i:])
+        v, offset = decode_packet(bits[i:])
         i += offset
-    return None, i
+        values.append(v)
+    return values, i
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        msg = f.readline().strip()
+        bits = hex_to_bin(f.readline().strip())
 
-    bits = hex_to_bin(msg)
     version_sum = 0
-    decode_packet(bits)
-    print(version_sum)
+    result = decode_packet(bits)[0]
+
+    print('part 1:', version_sum)
+    print('part 2:', result)
+
