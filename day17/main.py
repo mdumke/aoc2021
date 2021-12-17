531eee7d79827df29ccb8b1a8f965ef3c45a9ee2
Matthias Dumke <matthias.dumke@gmx.net>
2021-12-17 14:17:08 +0100

    use dataclass

--- day17/main.py
+++ day17/main.py
@@ -1,47 +1,58 @@
 """Day 17: Trick Shot"""
 
-from collections import namedtuple
+from dataclasses import dataclass
 
-Area = namedtuple('Area', 'x1 y1 x2 y2')
+@dataclass
+class Area:
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+
+    def contains(self, x, y):
+        return self.x1 <= x <= self.x2 and \
+               self.y2 <= y <= self.y1
+
+    def missed(self, x, y):
+        return x > self.x2 or y < self.y2
+
 
 def fire(vx, vy, target):
+    top = -10000
     x, y = 0, 0
-    max_y = -10_000
-    i = 0
-    while True:
+
+    for i in range(10000):
         x = x + vx
         y = y + vy
-        max_y = max(y, max_y)
-        vx = 0 if vx == 0 else vx - 1
+        top = max(y, top)
+        vx = max(0, vx - 1)
         vy -= 1
-        if target.x1 <= x <= target.x2 and \
-           target.y2 <= y <= target.y1:
-            return 'hit', max_y, i
-        if vx == 0 and x < target.x1:
-            return 'undershoot-x', max_y, i
-        if x > target.x2:
-            return 'overshoot-x', max_y, i
-        if vx == 0 and y < target.y2:
-            return 'overshoot-y', max_y, i
-        i += 1
+
+        if target.contains(x, y):
+            return 'hit', top
+
+        if target.missed(x, y):
+            return 'pass' if i == 0 else 'miss', top
+
 
 def find_hit_velocities(target):
     hits = []
+
     for vy in range(-100, 100):
-        vx = 1
-        while True:
-            response, max_y, i = fire(vx, vy, target)
+        for vx in range(1, 10000):
+            response, max_y = fire(vx, vy, target)
+
             if response == 'hit':
-                hits.append((max_y, vx, vy))
-            if response == 'overshoot-x' and i == 0:
+                hits.append(max_y)
+
+            if response == 'pass':
                 break
-            vx += 1
+
     return hits
 
 
 if __name__ == '__main__':
-    target = Area(236, -58, 262, -78)
-    hits = sorted(find_hit_velocities(target))
+    hits = sorted(find_hit_velocities(Area(236, -58, 262, -78)))
 
-    print('part 1:', hits[-1][0])
+    print('part 1:', hits[-1])
     print('part 2:', len(hits))
